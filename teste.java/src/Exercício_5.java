3fadf0bd16c66d7d5acced37f5adfe76a05de0d0
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-01 15:38:00 -0300

    Exercício 5

---
+++ teste.java/src/Exercício_5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Exercício_5 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite uma string: ");
+        String str = ler.nextLine();
+        System.out.println(limpaString(str));
+        System.out.println(isPalindromo(limpaString(str)));
+        ler.close();
+    }
+    public static String limpaString(String str){
+        String strLimpo = str.replaceAll("[ ,.;]", "");
+        strLimpo = strLimpo.toLowerCase();
+        return strLimpo;
+    }
+    public static boolean isPalindromo(String str){
+        if(str.length() <= 1){
+            return true;
+        }
+        else{
+            char pLetra = str.charAt(0);
+            char uLetra = str.charAt(str.length() - 1);
+            
+            if(pLetra == uLetra){
+                String nString = str.substring(1, str.length() - 1);
+                return isPalindromo(nString);
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
