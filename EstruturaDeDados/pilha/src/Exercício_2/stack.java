47fca355ebe85a8deff0777a429d1e94001db0db
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-15 19:39:18 -0300

    Formatando

--- EstruturaDeDados/pilha/src/Exercício_2/stack.java
+++ EstruturaDeDados/pilha/src/Exercício_2/stack.java
@@ -1,42 +1,49 @@
 package Exercício_2;
+
 import java.util.EmptyStackException;
+
 public class stack {
     private node top;
 
-    public stack(){
+    public stack() {
         this.top = null;
     }
-    public void push(char dado){
+
+    public void push(char dado) {
         node newNode = new node(dado);
         newNode.next = top;
         top = newNode;
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return top == null;
     }
-    public char pop(){
-        if(top == null){
+
+    public char pop() {
+        if (top == null) {
             throw new EmptyStackException();
         }
         char removeNode = top.value;
         top = top.next;
         return removeNode;
     }
-    public char peek(){
-        if(top == null){
+
+    public char peek() {
+        if (top == null) {
             throw new EmptyStackException();
         }
         return top.value;
     }
-    public String inverteString(String dado){
+
+    public String inverteString(String dado) {
         stack stack = new stack();
         String novaString = "";
-        for(char ch : dado.toCharArray()){
+        for (char ch : dado.toCharArray()) {
             stack.push(ch);
         }
-        do{
+        do {
             novaString += stack.pop();
-        }while(!stack.isEmpty());
+        } while (!stack.isEmpty());
         return novaString;
     }
 }
